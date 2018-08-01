d361a84dc752b888be63d1356846e48e67320b84
Áron Kovács <ironkovacs@gmail.com>
2018-08-01 16:36:26 +0200

    CORS

--- realeyeshomework/src/javascript/script.js
+++ realeyeshomework/src/javascript/script.js
@@ -187,5 +187,5 @@ toCurrency.addEventListener('change', () => {
 
 window.addEventListener('resize', () => { renderChart() });
 
-xhttp.open("GET", "https://crossorigin.me/http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml", true);
+xhttp.open("GET", "http://thingproxy.freeboard.io/fetch/http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml", true);
 xhttp.send();
