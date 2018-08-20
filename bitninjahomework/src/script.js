dedffb0f24699ca5e77eeb324e3ecf655f6758d8
Áron Kovács <ironkovacs@gmail.com>
2018-08-20 11:26:42 +0200

    frame

--- bitninjahomework/src/script.js
+++
@@ -1,9 +0,0 @@
-const logoStory = document.querySelector('.logo_sroty');
-const textStory = document.querySelector('.text_sroty');
-
-
-document.onload(() => {
-  logoStory.slick({
-    setting-name: setting-value
-  });
-});
